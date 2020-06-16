c9ae4be129ca22e2b45634b1053f528d137f66e1
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-06-16 15:42:44 +0700

    finish

--- app/src/main/java/com/example/stopwatchworkout/StopWatchActivity.java
+++ app/src/main/java/com/example/stopwatchworkout/StopWatchActivity.java
@@ -2,13 +2,64 @@ package com.example.stopwatchworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 
 public class StopWatchActivity extends AppCompatActivity {
+    
+    Button btnstart, btnstop;
+    ImageView icanchor;
+    Animation roundingalone;
+    Chronometer timeHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+        
+        btnstart = findViewById(R.id.btnstart);
+        btnstop = findViewById(R.id.btnstop);
+        icanchor = findViewById(R.id.icanchor);
+        timeHere = findViewById(R.id.timerHere);
+        
+        roundingalone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
+        
+        Typeface MMedium = Typeface.createFromAsset(getAssets(),"fonts/MMedium.ttf");
+        
+        btnstop.setAlpha(0);
+        
+        btnstart.setTypeface(MMedium);
+        btnstop.setTypeface(MMedium);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                icanchor.startAnimation(roundingalone);
+                btnstop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnstart.animate().alpha(0).setDuration(300).start();
+                
+                timeHere.setBase(SystemClock.elapsedRealtime());
+                timeHere.start();
+            }
+        });
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //stop animation
+                icanchor.clearAnimation();
+                btnstart.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnstop.animate().alpha(0).setDuration(300).start();
+                //stop timer
+                timeHere.stop();
+            }
+        });
     }
 }
