ef6399116be74ca861b53abda9d271b7301fbe90
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-06-16 14:34:45 +0700

    add OnCliskListener to move screen

--- app/src/main/java/com/example/stopwatchworkout/MainActivity.java
+++ app/src/main/java/com/example/stopwatchworkout/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.stopwatchworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         tvSplash.setTypeface(MRegular);
         tvSubSplash.setTypeface(MLight);
         btnget.setTypeface(MMedium);
+
+        btnget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent( MainActivity.this, StopWatchActivity.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
+            }
+        });
 
     }
 }
