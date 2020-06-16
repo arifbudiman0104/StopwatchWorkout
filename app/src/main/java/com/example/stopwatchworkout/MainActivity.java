9341287460da5a821ee86ca5e6310f7547ef8a16
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-06-16 14:01:28 +0700

    apply custom font

--- app/src/main/java/com/example/stopwatchworkout/MainActivity.java
+++ app/src/main/java/com/example/stopwatchworkout/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.stopwatchworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash, tvSubSplash;
+    Button btnget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+
+        Typeface MLight = Typeface.createFromAsset(getAssets(),"fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(),"fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(),"fonts/MRegular.ttf");
+
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        btnget.setTypeface(MMedium);
+
     }
 }
